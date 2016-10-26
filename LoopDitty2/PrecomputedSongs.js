17a112d49121399fd39ed2ac157064f161843b38
Chris Tralie <chris.tralie@gmail.com>
2016-10-25 21:39:36 -0400

    Adding the rest of the example songs

--- LoopDitty2/PrecomputedSongs.js
+++ LoopDitty2/PrecomputedSongs.js
@@ -31,3 +31,81 @@ function loadDiannaRoss() {
 function loadDrake() {
     loadPrecomputedSong("Examples/Drake.json");
 }
+function loadDuranDuran() {
+    loadPrecomputedSong("Examples/DuranDuran.json");
+}
+function loadEarthWindAndFire() {
+    loadPrecomputedSong("Examples/EarthWindAndFire.json");
+}
+function loadHudsonMohawke() {
+    loadPrecomputedSong("Examples/HudsonMohawke.json");
+}
+function loadJanetJackson() {
+    loadPrecomputedSong("Examples/JanetJackson.json");
+}
+function loadJenniferLopez() {
+    loadPrecomputedSong("Examples/JenniferLopez.json");
+}
+function loadKC() {
+    loadPrecomputedSong("Examples/KC.json");
+}
+function loadKevinLyttle() {
+    loadPrecomputedSong("Examples/KevinLyttle.json");
+}
+function loadMase() {
+    loadPrecomputedSong("Examples/Mase.json");
+}
+function loadMichaelJackson() {
+    loadPrecomputedSong("Examples/MichaelJackson.json");
+}
+function loadMikeJones() {
+    loadPrecomputedSong("Examples/MikeJones.json");
+}
+function loadMissyElliot(){
+    loadPrecomputedSong("Examples/MissyElliot.json");
+}
+function loadMotorhead() {
+    loadPrecomputedSong("Examples/Motorhead.json");
+}
+function loadPeterGabriel() {
+    loadPrecomputedSong("Examples/PeterGabriel.json");
+}
+function loadRickJames() {
+    loadPrecomputedSong("Examples/RickJames.json");
+}
+function loadRollingStones() {
+    loadPrecomputedSong("Examples/RollingStones.json");
+}
+function loadStevieWonder() {
+    loadPrecomputedSong("Examples/StevieWonder.json");
+}
+function loadSugarhillGang() {
+    loadPrecomputedSong("Examples/SugarhillGang.json");
+}
+function loadTchaikovsky() {
+    loadPrecomputedSong("Examples/Tchaikovsky.json");
+}
+function loadTeenaMarie() {
+    loadPrecomputedSong("Examples/TeenaMarie.json");
+}
+function loadTheBeatles() {
+    loadPrecomputedSong("Examples/TheBeatles.json");
+}
+function loadThePixies() {
+    loadPrecomputedSong("Examples/ThePixies.json");
+}
+function loadTI() {
+    loadPrecomputedSong("Examples/TI.json");
+}
+function loadVanHalen() {
+    loadPrecomputedSong("Examples/VanHalen.json");
+}
+function loadVanillaIce() {
+    loadPrecomputedSong("Examples/VanillaIce.json");
+}
+function loadWillSmith() {
+    loadPrecomputedSong("Examples/WillSmith.json");
+}
+function loadWizKhalifa() {
+    loadPrecomputedSong("Examples/WizKhalifa.json");
+}
